708e5f163bb0db4a8937cf1c98b0be91c044eabd
Gestalte <66849891+Gestalte@users.noreply.github.com>
2022-12-13 01:04:13 +0200

    Add new program code to parse and parse more SyntaxKinds.

--- CSharpCodeMapper/CSharpCodeMapper/Program.cs
+++ CSharpCodeMapper/CSharpCodeMapper/Program.cs
@@ -13,19 +13,70 @@ using System.Text;
 
 namespace HelloWorld
 {
-    class Program
-    {
-        private int Field = 0;
+void Main()
+{
+	Stock stock=new Stock(""THPW"");
+	stock.Price=27.10M;
+	stock.PriceChanged += stock_PriceChanged; // Subscribe to the event.
+	stock.Price=31.59M;
+}
 
-        private int Age{get;set;}
+// You can define other methods, fields, classes and namespaces here
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello, World!"");
-        }
+static void stock_PriceChanged(object sender,PriceChangedEventArgs e)
+{
+	if((e.NewPrice-e.LastPrice)/e.LastPrice>0.1M)
+	{
+		Console.WriteLine(""Alert, 10% stock price increase!"");
+	}
+}
 
-        private void DoSomething(int var1, string var 2){}
-    }
+// EventArgs is a base class for converying information for an event.
+public class PriceChangedEventArgs:System.EventArgs
+{
+	public readonly decimal LastPrice;
+	public readonly decimal NewPrice;
+	
+	public PriceChangedEventArgs(decimal lastPrice,decimal newPrice)
+	{
+		LastPrice = lastPrice;
+		NewPrice = newPrice;
+	}
+}
+
+public class Stock
+{
+	string symbol;
+	decimal price;
+
+	public Stock(string symbol) {this.symbol=symbol;}
+
+	public event Action<object, PriceChangedEventArgs> PriceChanged;
+	
+	// The pattern required that you write a protected virtual method that
+	// fires the event. The name must match the name of the event, prefixed
+	// with the word On, and then accept a single EventArgs argument.
+	protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+	{
+		// In multithreaded scenarios, you need to assign the delegate to a 
+		// temporary variable before testing and invoking it, to avoid an 
+		// obvious thread-safety error.
+		var temp=PriceChanged;
+		if(temp!=null) temp(this,e);
+	}
+	
+	public decimal Price
+	{
+		get{return price;}
+		set
+		{
+			if(price==value)return;
+			decimal oldPrice=price;
+			price=value;
+			OnPriceChanged(new PriceChangedEventArgs(oldPrice,price));
+		}
+	}
+}
 }";
 
 SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
@@ -34,35 +85,25 @@ CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
 
 void WriteStuff(SyntaxNode member, int indentLevel)
 {
-    string indent(string str)
-    {
-        var result = "";
-        for (int i = 0; i < indentLevel; i++)
-        {
-            result += "\t";
-        }
-        return result + str;
-    }
-
     switch (member.Kind())
     {
         case SyntaxKind.NamespaceDeclaration:
 
             var namespaceDeclarationSyntax = (NamespaceDeclarationSyntax)member;
-            WriteLine(indent(member.Kind() + " " + namespaceDeclarationSyntax.Name));
+            WriteLine(member.Kind() + " " + namespaceDeclarationSyntax.Name);
             
             break;
 
         case SyntaxKind.FileScopedNamespaceDeclaration:
 
             var fileScopedNamespaceDeclarationSyntax = (FileScopedNamespaceDeclarationSyntax)member;
-            WriteLine(indent(member.Kind() + " " + fileScopedNamespaceDeclarationSyntax.Name));
+            WriteLine(member.Kind() + " " + fileScopedNamespaceDeclarationSyntax.Name);
             break;
 
         case SyntaxKind.ClassDeclaration:
 
             var classDeclarationSyntax = (ClassDeclarationSyntax)member;
-            WriteLine(indent(member.Kind() + " " + classDeclarationSyntax.Identifier));
+            WriteLine(member.Kind() + " " + classDeclarationSyntax.Identifier);
             break;
 
         case SyntaxKind.StructDeclaration:
@@ -73,23 +114,32 @@ void WriteStuff(SyntaxNode member, int indentLevel)
         case SyntaxKind.FieldDeclaration:
 
             var FieldDeclarationSyntax = (FieldDeclarationSyntax)member;
-            FieldDeclarationSyntax.Declaration.Variables.ToList().ForEach(f => WriteLine(indent(member.Kind() + " " + f.Identifier)));
+            FieldDeclarationSyntax.Declaration.Variables.ToList().ForEach(f => WriteLine(member.Kind() + " " + f.Identifier));
             break;
 
-
         case SyntaxKind.EventFieldDeclaration:
+
+            var EventFieldDeclarationSyntax = (EventFieldDeclarationSyntax)member;
+            EventFieldDeclarationSyntax.Declaration.Variables.ToList().ForEach(f => WriteLine(member.Kind() + " " + f.Identifier));
+            break;
+
         case SyntaxKind.MethodDeclaration:
 
             var MethodDeclarationSyntax = (MethodDeclarationSyntax)member;
-            WriteLine(indent(member.Kind() + " " + MethodDeclarationSyntax.Identifier+ MethodDeclarationSyntax.ParameterList.GetText().ToString()));
+            WriteLine(member.Kind() + " " + MethodDeclarationSyntax.ReturnType.ToString() + " " + MethodDeclarationSyntax.Identifier+ MethodDeclarationSyntax.ParameterList.GetText().ToString());
             break;
 
         case SyntaxKind.ConstructorDeclaration:
+
+            var ConstructorDeclarationSyntax = (ConstructorDeclarationSyntax)member;
+            WriteLine(member.Kind() + " " + ConstructorDeclarationSyntax.Identifier + ConstructorDeclarationSyntax.ParameterList.GetText().ToString());
+            break;
+
         case SyntaxKind.DestructorDeclaration:
         case SyntaxKind.PropertyDeclaration:
 
             var PropertyDeclarationSyntax = (PropertyDeclarationSyntax)member;
-            WriteLine(indent(member.Kind() + " " + PropertyDeclarationSyntax.Identifier));
+            WriteLine(member.Kind() + " " + PropertyDeclarationSyntax.Identifier);
             break;
 
         case SyntaxKind.EventDeclaration:
